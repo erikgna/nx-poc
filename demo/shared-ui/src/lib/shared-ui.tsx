5e1346846e76ac8461eed9ddfc552e81d9b35528
Erik Na <erikgnaa@gmail.com>
2025-11-12 20:51:34 -0300

    first

---
+++ demo/shared-ui/src/lib/shared-ui.tsx
@@ -0,0 +1,15 @@
+
+
+
+export function DemoSharedUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to DemoSharedUi!</h1>
+    </div>
+  );
+}
+
+export default DemoSharedUi;
+
+
+
